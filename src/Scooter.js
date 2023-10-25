6bb079fcea0bc275e9673d662ca7189e281ef3f9
Laila Case <lai_case@hotmail.com>
2023-10-25 15:28:38 +0100

    Add TDD for properties of Scooter class

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,6 +1,13 @@
-class Scooter{
+class Scooter {
   // scooter code here
+  constructor(station, user, serial, nextSerial, charge, isBroken) {
+    this.station = station;
+    this.user = user;
+    this.serial = 0;
+    this.nextSerial = 1;
+    this.charge = 100;
+    this.isBroken = false;
+  }
 }
 
-
-module.exports = Scooter
+module.exports = Scooter;
--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,25 +1,26 @@
-const Scooter = require('../src/Scooter')
-const User = require('../src/User')
+const Scooter = require("../src/Scooter");
+const User = require("../src/User");
 
 //typeof scooter === object
-describe('scooter object', () => {
-  test('Scooter class should create Scooter instance', () => {
+describe("Scooter is object", () => {
+  test("Scooter class should create Scooter instance", () => {
     const scooter = new Scooter();
     expect(scooter).toBeInstanceOf(Scooter);
   });
-})
+});
+
+describe("All properties exist", () => {
+  test("should have the correct properties and corresponding data types on initialisation", () => {
+    const testScooter = new Scooter();
+    expect(testScooter).toHaveProperty("station");
+    expect(typeof testScooter.serial).toBe("number");
+    expect(typeof testScooter.nextSerial).toBe("number");
+    expect(testScooter.charge).toBe(100);
+    expect(testScooter.isBroken).toBe(false);
+  });
+});
+
+describe("Name of the group", () => {});
 
 //Method tests
-describe('scooter methods', () => {
-  // tests here!
-
-  //rent method
-  
-
-  //dock method
-
-  //requestRepair method
-
-  //charge method
-
-})
+describe("scooter methods", () => {});
